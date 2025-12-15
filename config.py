e0b431602799dec4efe98339d588f5dde4cae925
Chintu0013 <zbcdabcd1290@gmail.com>
2025-12-15 11:57:42 +0530

    Update API_ID and API_HASH in config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-API_ID = os.environ.get("API_ID", "26468828")
+API_ID = os.environ.get("API_ID", "31193989")
 
-API_HASH = os.environ.get("API_HASH", "4693513c08d1ac6af15f95b116c29478")
+API_HASH = os.environ.get("API_HASH", "9bbb821c39e6b0b8a6fe7d901666ecdf")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
